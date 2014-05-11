8f90cda007f90a854874b91139518f8818964e86
macair12 <macair12@10.211.55.3>
2014-05-11 16:28:29 +0700

    edit index

--- public/index.php
+++ public/index.php
@@ -1,26 +1,16 @@
-<<<<<<< HEAD
-<?php 
-
-
-echo "test edit";
-
-
-=======
 <?php
-
 try {
-
-    //Register an autoloader
-    $loader = new \Phalcon\Loader();
-    $loader->registerDirs(array(
-        '../app/controllers/',
-        '../app/models/'
-    ))->register();
+	
+	// Register an autoloader
+	$loader = new \Phalcon\Loader ();
+	$loader->registerDirs ( array (
+			'../app/controllers/',
+			'../app/models/' 
+	) )->register ();
 	
 	// Create a DI
 	$di = new Phalcon\DI\FactoryDefault ();
-	
-	//Setup the database service
+	// Setup the database service
 	$di->set ( 'db', function () {
 		return new \Phalcon\Db\Adapter\Pdo\Mysql ( array (
 				"host" => "localhost",
@@ -29,41 +19,30 @@ try {
 				"dbname" => "estudent" 
 		) );
 	} );
-    
-    
-    //Setup the view component
-   /* $di->set('view', function(){
-        $view = new \Phalcon\Mvc\View();
-        $view->setViewsDir('../app/views/');
-        return $view;
-    });*/
-    	//Registering Volt as template engine
-    	$di->set('view', function() {
-    	
-    		$view = new \Phalcon\Mvc\View();
-    	
-    		$view->setViewsDir('../app/views/');
-    	
-    		$view->registerEngines(array(
-    				".volt" => 'Phalcon\Mvc\View\Engine\Volt'
-    		));
-    	
-    		return $view;
-    	});
-
-    //Setup a base URI so that all generated URIs include the "tutorial" folder
-    $di->set('url', function(){
-        $url = new \Phalcon\Mvc\Url();
-        $url->setBaseUri('/tutorial/');
-        return $url;
-    });
-
-    //Handle the request
-    $application = new \Phalcon\Mvc\Application($di);
-
-    echo $application->handle()->getContent();
-
-} catch(\Phalcon\Exception $e) {
-     echo "PhalconException: ", $e->getMessage();
+	
+	//Registering Volt as template engine
+	$di->set('view', function() {
+			$view = new \Phalcon\Mvc\View();
+			$view->setViewsDir('../app/views/');
+			$view->registerEngines(array(
+					".volt" => 'Phalcon\Mvc\View\Engine\Volt'
+			));
+			return $view;
+		});
+	
+	
+	
+	// Setup a base URI so that all generated URIs include the "tutorial" folder
+	$di->set ( 'url', function () {
+		$url = new \Phalcon\Mvc\Url ();
+		$url->setBaseUri ( '/tutorial/' );
+		return $url;
+	} );
+	
+	// Handle the request
+	$application = new \Phalcon\Mvc\Application ( $di );
+	
+	echo $application->handle ()->getContent ();
+} catch ( \Phalcon\Exception $e ) {
+	echo "PhalconException: ", $e->getMessage ();
 }
->>>>>>> branch 'master' of https://github.com/nung1com/estudent.git
